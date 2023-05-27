4b649a4892a3c86f1e619589280c4958d928d7fe
zer0231 <pranay_zer0@protonmail.com>
2023-05-27 22:45:20 +0545

    Added layout and viewbinding

--- app/src/main/java/com/zero/synefiliya/activities/MainActivity.kt
+++ app/src/main/java/com/zero/synefiliya/activities/MainActivity.kt
@@ -2,13 +2,20 @@ package com.zero.synefiliya.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.zero.synefiliya.R
+import com.zero.synefiliya.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+    val TAG = "MAIN_ACTIVITY"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
+
 }
