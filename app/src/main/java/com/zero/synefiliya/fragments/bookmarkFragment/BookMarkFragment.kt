dc98072cf87615d65a9f416deb8c74d499fe84c6
zer0231 <pranay_zer0@protonmail.com>
2023-06-09 22:04:34 +0545

    Added bookmark option

--- app/src/main/java/com/zero/synefiliya/fragments/bookmarkFragment/BookMarkFragment.kt
+++ app/src/main/java/com/zero/synefiliya/fragments/bookmarkFragment/BookMarkFragment.kt
@@ -5,35 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import com.zero.synefiliya.databinding.FragmentBookmarkBinding
-import com.zero.synefiliya.fragments.dashboardFragment.models.MovieDetail
-import com.zero.synefiliya.utils.roomdb.MovieDatabase
 
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-@AndroidEntryPoint
 class BookMarkFragment : Fragment() {
-    private var _binding: FragmentBookmarkBinding = null!!
-    private val binding = _binding
+    private var _binding: FragmentBookmarkBinding? = null
+   private val binding get() =  _binding!!
 
-    @Inject
-    lateinit var movieDB: MovieDatabase
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentBookmarkBinding.inflate(inflater, container, false)
-        lifecycleScope.launch(Dispatchers.IO) {
-            //TODO: CREATE ADAPTER AND ASSIGN TO RecyclerView FROM THIS COROUTINE
-//            getBookMarkedList()
-        }
+
         return binding.root
     }
 
-//    private suspend fun getBookMarkedList(): List<MovieDetail> {
-//        return movieDB.movieDao().getAllMovies()
-//    }
+
 }
